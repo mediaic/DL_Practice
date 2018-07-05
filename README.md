ae6ad79876261307d97ee75704d988189649aefe
Ho Hsuan-I <azuxmioy@gmail.com>
2018-07-05 14:51:09 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# DL_Practice
+DL_Practice
+===
 Deep learning practice for students to play with popular deep learning frameworks.
+
+## Description
+The practice includes 2 different tasks
+* Classification : Students will train a simple neural network classifier and play with many training tricks.
+* Segmentation : Students will learn the skills of transfer learning using pre-trained CNNs.
+
+## Rules
+Students are required to finish their works using either [PyTorch](https://github.com/pytorch/pytorch) or [TensorFlow](https://github.com/tensorflow/tensorflow). We encourage you to learn [PyTorch](https://github.com/pytorch/pytorch) if you are the beginner to deep learning since we provide detailed tutorial in the crash courses.
+
+## Prerequisites
+* python3.5+
+* CUDA 9.0
+* cuDNN 7
+* TensorFlow 1.6: pip3 install tensorflow-gpu==1.6
+or
+* Pytorch 0.4.0: 
+pip3 install http://download.pytorch.org/whl/cu90/torch-0.4.0-cp35-cp35m-linux_x86_64.whl 
+pip3 install torchvision
+
+You can also use the insall script in Ubuntu 16.04 LTS.
---
+++ install.sh
@@ -0,0 +1,26 @@
+##########################################################
+# ****** This script is ONLY for Ubuntu 16.04 LTS ****** #
+##########################################################
+
+############ Cuda setup ############
+# Download specified CUDA driver package
+CUDA_REPO_PKG=cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+sudo wget -O ${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG} 
+# Register driver package on your machine
+sudo dpkg -i ${CUDA_REPO_PKG}
+sudo apt-get update
+# install CUDA driver
+sudo apt-get install cuda-drivers
+sudo apt-get install cuda=9.0.176-1
+# Register & install CuDNN library
+sudo dpkg -i libcudnn7_7.0.5.15-1+cuda9.0_amd64.deb
+sudo apt install nvidia-cuda-toolkit
+
+############ Upgrade to python3.6 ############
+# upgrade pip
+sudo pip3 install --upgrade pip
+# install tensorflow 1.6
+sudo pip3 install tensorflow-gpu==1.6
+# install pytorch 0.3.1
+sudo pip3 install http://download.pytorch.org/whl/cu90/torch-0.4.0-cp35-cp35m-linux_x86_64.whl
+sudo pip3 install torchvision
